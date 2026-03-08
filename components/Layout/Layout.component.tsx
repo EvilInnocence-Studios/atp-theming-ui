ed269080aa6af6af21391ff481ee46403873e639
Andrew Wittrock <andy@wittrock.us>
2026-03-08 14:44:59 -0500

    Remove debugging

--- components/Layout/Layout.component.tsx
+++ components/Layout/Layout.component.tsx
@@ -3,7 +3,7 @@ import { LayoutComponent as LayoutComponentImpl } from "@theming/components/Layo
 import { LayoutProps } from "./Layout.d";
 
 export const LayoutComponent = overridable(({ component, Provider, context }: LayoutProps) =>
-    component && Provider ? <Provider value={context}>{JSON.stringify(context)}<LayoutComponentImpl {...component} /></Provider> :
+    component && Provider ? <Provider value={context}><LayoutComponentImpl {...component} /></Provider> :
     component             ? <LayoutComponentImpl {...component} /> :
                             null
 );
