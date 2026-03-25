13ae2cb1fb35a22c42c5a2f23652f767fba85d5d
Andrew Wittrock <andy@wittrock.us>
2026-03-25 12:14:09 -0500

    Revert layout editor update

--- components/LayoutManager/LayoutManager.component.tsx
+++ components/LayoutManager/LayoutManager.component.tsx
@@ -1,16 +1,21 @@
 import { Editable } from "@core/components/Editable";
 import { Label } from "@core/components/Label";
 import { overridable } from "@core/lib/overridable";
+import { useDroppable } from "@dnd-kit/core";
 import { faPaintRoller } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { LayoutRegistry } from "@theming/lib/layout/componentRegistry";
+import { SelectableItem } from "@theming/components/SlotRenderer/SlotRenderer.component";
+import slotStyles from "@theming/components/SlotRenderer/SlotRenderer.module.scss";
+import { ComponentRegistry, LayoutRegistry } from "@theming/lib/layout/componentRegistry";
+import { getAncestryPath } from "@theming/lib/layout/utils";
 import { Col, Row, Select, Switch } from "antd";
+import { Breadcrumb } from "./Breadcrumb.component";
 import { LayoutManagerProps } from "./LayoutManager.d";
 import styles from './LayoutManager.module.scss';
 import { PropertyPanel } from "./PropertyPanel";
 
 import { ComponentLibrary } from "./ComponentLibrary.component";
-import { LayoutEditor } from "./LayoutEditor.component";
+import { JsonLayoutDisplay } from "./JsonLayoutDisplay.component";
 
 // ... imports
 import { ConfigProvider } from "antd";
@@ -18,6 +23,7 @@ import { useTheme } from "@theming/lib/useTheme";
 import { objMap } from "ts-functional";
 import { IStyleVar } from "@theming/components/Style/Style.d";
 import { ILayoutComponent } from "@theming/lib/layout/layout";
+import clsx from "clsx";
 
 // ... existing code ...
 
@@ -27,6 +33,11 @@ export const LayoutManagerComponent = overridable(({
     selectComponent, removeComponent, updateComponent,
     classes = styles, UpdateButtons,
 }:LayoutManagerProps) => {
+    const { setNodeRef, isOver } = useDroppable({
+        id: 'root-layout',
+        disabled: !!layout && !!layout.component,
+    });
+
     // Find the Style layout to get global vars
     const styleLayout = theme && theme.json ? Object.values(theme.json).find((c:any) => c.component === "Style") as ILayoutComponent : null;
     const styleVars = styleLayout?.props?.vars;
@@ -67,16 +78,41 @@ export const LayoutManagerComponent = overridable(({
                             ${Object.values(objMap<IStyleVar, string>(v => `--${v.name}: ${v.value};`)(styleVars || {})).join("\n")}
                         }`}
                     </style>
-                    <LayoutEditor
-                        layout={layout}
-                        isEditing={isEditing}
-                        showJson={showJson}
-                        selectedId={selectedId}
-                        selectComponent={selectComponent}
-                        removeComponent={removeComponent}
-                        updateComponent={updateComponent}
-                        classes={classes}
-                    />
+                    {layout && layout.component && isEditing?.isset && (
+                        <Breadcrumb 
+                            path={selectedId ? getAncestryPath(layout, selectedId) : [layout]} 
+                            onSelect={selectComponent}
+                        />
+                    )}
+                    {layout && layout.component ? (() => {
+                        const Component = ComponentRegistry.get(layout.component)?.component;
+                        const __update = (key:string) => (value:any) => {
+                            if (layout.id) updateComponent(layout.id, {props: {...layout.props, [key]: value }});
+                        };
+                        const rootContent = Component ? <Component {...layout.props} slots={layout.slots} __layoutId={layout.id} css={layout.css} __update={__update} __isSelected={selectedId === layout.id} /> : null;
+                    
+                        if (rootContent) {
+                            return (
+                                <>
+                                    <SelectableItem
+                                        id={layout.id!}
+                                        selected={selectedId === layout.id}
+                                        title={layout.name || layout.component}
+                                        onSelect={() => selectComponent(layout.id!)}
+                                        onDelete={() => removeComponent(layout.id!)}
+                                        classes={slotStyles} // Use SlotRenderer styles for the item
+                                        className={clsx(slotStyles.item, "layout-editor-canvas")}
+                                    >
+                                        {rootContent}
+                                    </SelectableItem>
+                                    <JsonLayoutDisplay layout={layout} showJson={showJson} />
+                                </>
+                            );
+                        }
+                        return null;
+                    })() : <div ref={setNodeRef} className={clsx(classes.rootDropTarget, { [classes.active]: isOver })}>
+                        Drop a component here to start
+                    </div>}
                 </ConfigProvider>
             </Col>
             {isEditing?.isset && <Col span={6}>
