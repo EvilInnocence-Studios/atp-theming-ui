3a9ca29dd25d1d018e2f48a39f37c6d67244adb2
Andrew Wittrock <andy@wittrock.us>
2026-03-29 14:09:35 -0500

    Update UX on layout selector

--- components/LayoutManager/LayoutManager.component.tsx
+++ components/LayoutManager/LayoutManager.component.tsx
@@ -4,7 +4,8 @@ import { overridable } from "@core/lib/overridable";
 import { faPaintRoller } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { LayoutRegistry } from "@theming/lib/layout/componentRegistry";
-import { Col, Row, Select } from "antd";
+import { Button, Col, Modal, Row } from "antd";
+import { useState } from "react";
 import { LayoutEditor, LayoutEditorProvider } from "./LayoutEditor";
 import { LayoutManagerProps } from "./LayoutManager.d";
 import styles from './LayoutManager.module.scss';
@@ -13,6 +14,10 @@ export const LayoutManagerComponent = overridable(({
     theme, updater, element, setElement, layout, onChange, UpdateButtons,
     classes = styles,
 }:LayoutManagerProps) => {
+    const [isElementModalOpen, setIsElementModalOpen] = useState(false);
+
+    const options = LayoutRegistry.getOptions();
+    const currentOption = options.find(o => o.value === element);
 
     return <div className={classes.layoutManager}>
         <Row gutter={[16,16]}>
@@ -22,7 +27,40 @@ export const LayoutManagerComponent = overridable(({
                 </h1>
             </Col>
             <Col span={6}>
-                <Select className={classes.layoutElementSelect} options={LayoutRegistry.getOptions()} value={element} onChange={setElement} />
+                <Button 
+                    className={classes.layoutElementSelect} 
+                    onClick={() => setIsElementModalOpen(true)}
+                >
+                    <div style={{ textAlign: "left", width: "100%" }}>
+                        {currentOption ? currentOption.label : "Select Layout Element"}
+                    </div>
+                </Button>
+                <Modal
+                    title="Select Layout Element"
+                    open={isElementModalOpen}
+                    onCancel={() => setIsElementModalOpen(false)}
+                    footer={null}
+                    width={1000}
+                >
+                    <Row
+                        gutter={[16,16]}
+                        className={classes.optionList}
+                    >
+                        {options.map(item => (
+                            <Col
+                                span={8}
+                                key={item.value}
+                            >
+                                <div
+                                    className={`${classes.optionItem} ${item.value === element ? classes.selectedOption : ''}`}
+                                    onClick={() => { setElement(item.value); setIsElementModalOpen(false); }}
+                                >
+                                    {item.label}
+                                </div>
+                            </Col>
+                        ))}
+                    </Row>
+                </Modal>
             </Col>
             <Col span={8}>
                 {theme && updater && <Label label="Theme Name"><Editable value={theme.name || ""} onChange={updater.updateString("name")} /></Label>}
