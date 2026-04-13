70748a284b9f858493658675e5276a920347160a
Andrew Wittrock <andy@wittrock.us>
2026-04-13 14:49:58 -0500

    Fix incorrect theme load call

--- lib/useTheme.ts
+++ lib/useTheme.ts
@@ -1,20 +1,45 @@
 import { ITheme } from "@common-shared/theme/types";
 import { useSetting } from "@common/lib/setting/services";
+import { services } from "@core/lib/api";
 import { IStyleVar } from "@theming/components/Style/Style";
 import { ThemeConfig, theme as antTheme } from "antd";
+import { useEffect } from "react";
+import { memoizePromise } from "ts-functional";
 import { Index } from "ts-functional/dist/types";
 import { useSharedState } from "unstateless";
-import { useEffect } from "react";
-import { services } from "@core/lib/api";
+
+const getTheme = memoizePromise((id:string) => services().theme.get(id), {});
+
+const useCurrentId = useSharedState<string>("");
+const useCurrentTheme = useSharedState<ITheme | null>(null);
+
+// export const useLayoutTheme = () => {
+//     const defaultThemeId = useSetting("defaultThemeId");
+//     const [currentThemeId, setCurrentThemeId] = useCurrentId(defaultThemeId);
+//     const [theme, setTheme] = useCurrentTheme();
+
+//     useEffect(() => {
+//         if (!currentThemeId) return;
+//         getTheme(currentThemeId).then(setTheme);
+//     }, [currentThemeId]);
+    
+//     return { theme, onChange: setCurrentThemeId };
+// }
 
 export const useLayoutTheme = () => {
     const defaultThemeId = useSetting("defaultThemeId");
-    const [currentThemeId, setCurrentThemeId] = useSharedState<string>("currentThemeId", defaultThemeId)();
-    const [theme, setTheme] = useSharedState<ITheme | null>("currentThemeId", null)();
+    const [currentThemeId, setCurrentThemeId] = useCurrentId();
+    const [theme, setTheme] = useCurrentTheme();
 
     useEffect(() => {
-        if (!defaultThemeId) return;
-        services().theme.get(currentThemeId).then(setTheme);
+        if(!currentThemeId && defaultThemeId) {
+            setCurrentThemeId(defaultThemeId);
+        }
+    }, [defaultThemeId]);
+    
+    useEffect(() => {
+        if (!currentThemeId) return;
+        getTheme(currentThemeId).then(setTheme);
     }, [currentThemeId]);
     
     return { theme, onChange: setCurrentThemeId };
