83f07be2d2c9b7e03189b9603daa8d367fa3e7c5
Andrew Wittrock <andy@wittrock.us>
2026-04-17 15:19:40 -0500

    Remove fixed from layouts

--- components/Layout/Layout.component.tsx
+++ components/Layout/Layout.component.tsx
@@ -2,6 +2,6 @@ import { overridable } from "@core/lib/overridable";
 import { LayoutComponent as LayoutComponentImpl } from "@theming/components/LayoutComponent";
 import { LayoutProps } from "./Layout.d";
 
-export const LayoutComponent = overridable(({ component, __fixed }: LayoutProps) =>
-    component ? <LayoutComponentImpl {...component} __fixed={__fixed} /> : null
+export const LayoutComponent = overridable(({ component }: LayoutProps) =>
+    component ? <LayoutComponentImpl {...component} __fixed={true} /> : null
 );
