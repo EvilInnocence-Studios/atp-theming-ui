1b799918b41f32096ecefa49f1e12251ceef5917
Andrew Wittrock <andy@wittrock.us>
2026-04-23 13:37:48 -0500

    Fix raw prop

--- components/Text/Text.props.tsx
+++ components/Text/Text.props.tsx
@@ -2,13 +2,13 @@ import { MarkdownEditor } from "@core/components/MarkdownEditor";
 import { Card, Checkbox } from "antd";
 import { ITextInputProps } from "./Text";
 
-export const TextPropEditor = ({markdown, inline}: ITextInputProps, _updateProps: (props: any) => void, updateProp: (prop: string) => (value: any) => void) => {
+export const TextPropEditor = ({markdown, inline, raw}: ITextInputProps, _updateProps: (props: any) => void, updateProp: (prop: string) => (value: any) => void) => {
     return <>
         <Card size="small" title="Text">
             <MarkdownEditor value={markdown || ""} onChange={(markdown) => updateProp("markdown")(markdown)}/>
             <br/>
             <Checkbox checked={inline} onChange={(e) => updateProp("inline")(e.target.checked)} >Inline</Checkbox>
-            <Checkbox checked={inline} onChange={(e) => updateProp("raw")(e.target.checked)} >Raw</Checkbox>
+            <Checkbox checked={raw} onChange={(e) => updateProp("raw")(e.target.checked)} >Raw</Checkbox>
         </Card>
     </>;
 };
